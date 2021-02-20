f1e903d44db40726029de91e39cf8f340425cc33
Cristian Gonzalez <cristian3629@gmail.com>
2021-02-20 20:24:18 -0300

    add aux funcs

--- basic.clj
+++ basic.clj
@@ -713,6 +713,16 @@
 ; user=> (cargar-linea '(15 (X = X - 1)) ['((10 (PRINT X)) (15 (X = X + 1)) (20 (X = 100))) [:ejecucion-inmediata 0] [] [] [] 0 {}])
 ; [((10 (PRINT X)) (15 (X = X - 1)) (20 (X = 100))) [:ejecucion-inmediata 0] [] [] [] 0 {}]
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn obtener-linea-codigo [linea]
+    (first linea)
+      )
+
+
+(defn obtener-ambiente [amb]
+      (first amb)
+      )
+
 (defn cargar-linea [linea amb]
       )
 
